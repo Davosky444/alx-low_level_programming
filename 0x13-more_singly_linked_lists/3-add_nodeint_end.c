b2ebe3d975b7af44aa187c53fcb7aeda524461d6
Idoko David Edward <idoko.unijosbch@gmail.com>
2022-10-24 11:53:19 -0700

    task

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -31,20 +31,3 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	tmp->next = newNode;
 	return (newNode);
 }
-Footer
-© 2022 GitHub, Inc.
-Footer navigation
-
-    Terms
-    Privacy
-    Security
-    Status
-    Docs
-    Contact GitHub
-    Pricing
-    API
-    Training
-    Blog
-    About
-
-
